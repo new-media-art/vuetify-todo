374929641c5d49e63b3ac82be6c77a7c5008fcdc
Ulrich Fladl <ulricof@gmail.com>
2022-04-13 15:31:12 +0200

    video26

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,37 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    tasks: [
+      {
+       id: 1,
+       title: 'wake up',
+       done: false
+     },
+       {
+       id: 2,
+       title: 'eat Lunch',
+       done: false
+     },
+       {
+       id: 3,
+       title: 'whatch tv',
+       done: false
+     },
+   ]
   },
   getters: {
   },
   mutations: {
+    addTask(state, newTaskTitle){
+      let newTask = {
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      state.tasks.push(newTask)
+     
+    },
   },
   actions: {
-  },
-  modules: {
   }
 })
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,24 +1,75 @@
 <template>
   <div class="home">
+          <v-text-field
+          v-model="newTaskTitle"
+          @click:append="addTask"
+          @keyup.enter="addTask"
+            class="pa-3"
+            outlined
+            label="Add task"
+            append-icon="mdi-plus"
+            hide-details
+            clearable
+          ></v-text-field>
+
     <v-list
+      v-if="$store.state.tasks.length"
+      class="pt-0"
       flat
     >
-        <v-list-item>
-          <template v-slot:default="{ active, }">
+      <div
+        v-for="task in $store.state.tasks"
+          :key="task.id"
+      >
+          <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5' : task.done }"
+          >
+          <template v-slot:default>
             <v-list-item-action>
               <v-checkbox
-                :input-value="active"
+                :input-value="task.done"
                 color="primary"
               ></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>Wake up</v-list-item-title>
+              <v-list-item-title
+              :class="{ 'text-decoration-line-through' : task.done }"
+              >
+                {{  task.title }}
+                </v-list-item-title>
               
             </v-list-item-content>
-          </template>
+          
+
+            <v-list-item-action>
+            <v-btn 
+            @click.stop="deleteTask(task.id)"
+            icon
+            >
+              <v-icon color="grey primary-1">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+
+        </template>
         </v-list-item>
+        <v-divider></v-divider>
+      </div>
+      
     </v-list>
+    <div
+    v-else
+    class="no-tasks"
+    >
+    <v-icon
+      size="100"
+      color="primary"
+    >
+      mdi-check
+    </v-icon>
+    <div class="text-h5 primary--text">No tasks</div>   
+    </div>
   </div>
 </template>
 
@@ -29,24 +80,31 @@
     name: 'Home',
     data() {
       return {
-        tasks: [
-        {
-          id: 1,
-          title: 'wake up'
-        },
-          {
-          id: 2,
-          title: 'eat Lunch'
-        },
-          {
-          id: 3,
-          title: 'whatch tv'
-        },
-      ]
+        newTaskTitle: ''
+     
       }
       
+    },
+    methods: {
+    
+      doneTask(id){
+        let task = this.tasks.filter(task => task.id === id)[0]
+        task.done = !task.done
+      },
+      deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+      }
     }
 
   
   }
 </script>
+
+<style lang="sass">
+  .no-tasks 
+    position: absolute
+    left: 50%
+    top: 50%
+    transform: translate(-50%, -50%)
+    opacity: 0.5
+</style>
