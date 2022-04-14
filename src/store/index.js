e019da1470cd2f317a15d6a8647eb6b47ee0426f
Ulrico Carmona <ulricocarmona@gmail.com>
2022-04-14 22:55:11 +0200

    addTask not working

--- src/store/index.js
+++ src/store/index.js
@@ -1,48 +1,45 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     tasks: [
       {
-       id: 1,
-       title: 'wake up',
-       done: false
-     },
-       {
-       id: 2,
-       title: 'eat Lunch',
-       done: false
-     },
-       {
-       id: 3,
-       title: 'whatch tv',
-       done: false
-     },
-   ]
+        id: 1,
+        title: "wake up",
+        done: false,
+      },
+      {
+        id: 2,
+        title: "eat Lunch",
+        done: false,
+      },
+      {
+        id: 3,
+        title: "whatch tv",
+        done: false,
+      },
+    ],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
-    addTask(state, newTaskTitle){
+    addTask(state, newTaskTitle) {
       let newTask = {
         id: Date.now(),
         title: newTaskTitle,
-        done: false
-      }
-      state.tasks.push(newTask)
-     
+        done: false,
+      };
+      state.tasks.push(newTask);
     },
-    doneTask(state, id){
-      let task = state.tasks.filter(task => task.id === id)[0]
-      task.done = !task.done
+    doneTask(state, id) {
+      let task = state.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
     },
     deleteTask(state, id) {
-      state.tasks = state.tasks.filter(task => task.id !== id)
-      }
+      state.tasks = state.tasks.filter((task) => task.id !== id);
+    },
   },
-  actions: {
-  }
-})
+  actions: {},
+});
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,63 +1,10 @@
 <template>
   <div class="home">
-          <v-text-field
-          v-model="newTaskTitle"
-          @click:append="addTask" 
-          @keyup.enter="addTask"
-            class="pa-3"
-            outlined
-            label="Add task"
-            append-icon="mdi-plus"
-            hide-details
-            clearable
-          ></v-text-field>
+        <field-add-task />
 
-    <v-list
-      v-if="$store.state.tasks.length"
-      class="pt-0"
-      flat
-    >
-      <div
-        v-for="task in $store.state.tasks"
-          :key="task.id"
-      >
-          <v-list-item
-          @click="$store.commit('doneTask', task.id)"
-          :class="{ 'blue lighten-5' : task.done }"
-          >
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox
-                :input-value="task.done"
-                color="primary"
-              ></v-checkbox>
-            </v-list-item-action>
-
-            <v-list-item-content>
-              <v-list-item-title
-              :class="{ 'text-decoration-line-through' : task.done }"
-              >
-                {{  task.title }}
-                </v-list-item-title>
-              
-            </v-list-item-content>
-          
-
-            <v-list-item-action>
-            <v-btn 
-            @click.stop="$store.commit('deleteTask', task.id)"
-            icon
-            >
-              <v-icon color="grey primary-1">mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-
-        </template>
-        </v-list-item>
-        <v-divider></v-divider>
-      </div>
-      
-    </v-list>
+        <list-tasks 
+          v-if="$store.state.tasks.length"
+        />
     <div
     v-else
     class="no-tasks"
@@ -78,19 +25,15 @@
 
   export default {
     name: 'Home',
-    data() {
-      return {
-        newTaskTitle: ''
-     
-      }
-      
-    },
+   
     methods: {
       addTask(){
        this.$store.commit('addTask', this.newTaskTitle)
        this.newTaskTitle = ''
       },
-      
+      components: { 
+        'field-add-task': require('@/components/Todo/FieldAddTask.vue').default,
+        'list-tasks': require('@/components/Todo/ListTasks.vue').default
       
     }
 
